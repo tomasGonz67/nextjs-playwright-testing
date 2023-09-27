0e0a917f61a0dca84cc4a34bae669ed3dfd720d6
kadeem-lewis <email@kadeemlewis.com>
2023-09-27 00:44:22 -0400

    created tests for 3rd section of tutorial

---
+++ tests/assets-metadata-css.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+const localhost = "http://localhost:3000";
+
+test("first post has title", async ({ page }) => {
+  await page.goto(`${localhost}/posts/first-post`);
+  await expect(page).toHaveTitle("First Post");
+});
+
+test("layout has css applied", async ({ page }) => {
+  await page.goto(localhost);
+  await expect(page.locator("body>div>div").first()).toHaveClass(
+    /layout_container.*/
+  );
+});
+
+test("global css applied", async ({ page }) => {
+  await page.goto(localhost);
+  await expect(page.locator("body")).toHaveCSS("font-size", "18px");
+});
+
+test("layout back to home button works", async ({ page }) => {
+  await page.goto(`${localhost}/posts/first-post`);
+  await page.locator("div>div>a").click();
+  await expect(page).toHaveURL(localhost);
+});
+
+test("name was updated", async ({ page }) => {
+  await page.goto(localhost);
+  await expect(page.locator("header > h1")).not.toHaveText("Your Name");
+});
+
+test("description was updated", async ({ page }) => {
+  await page.goto(localhost);
+  await expect(page.locator("main>section>p").first()).not.toHaveText(
+    "[Your Self Introduction]"
+  );
+});
