0396778c97c618a279df7a6064536030135f4af6
kadeem-lewis <email@kadeemlewis.com>
2023-09-27 01:25:48 -0400

    updated api test syntax

--- tests/api-routes.spec.js
+++ tests/api-routes.spec.js
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 
 const localhost = "http://localhost:3000";
 
-test("simple api endpoint", async ({ page }) => {
-  const response = await page.request.get(`${localhost}/api/hello`);
-  await expect(response).toBeOK();
+test("simple api endpoint", async ({ request }) => {
+  const response = await request.get(`${localhost}/api/hello`);
+  expect(response.ok()).toBeTruthy();
 
   const responseBody = await response.json();
   expect(responseBody).toHaveProperty("text", "Hello");
