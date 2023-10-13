2adf222a8363a2b4cd91cfd30782ea7272ba41d5
kadeem-lewis <email@kadeemlewis.com>
2023-10-13 13:30:37 -0400

    created tests for dynamic routes section of tutorial

--- tests/dynamic-routes.spec.js
+++ tests/dynamic-routes.spec.js
@@ -1,7 +1,34 @@
 import { test, expect } from "@playwright/test";
+import exp from "constants";
 
-test("date properly formatted", async ({ page }) => {});
+const localhost = "http://localhost:3000";
 
-test("post contains post data", async ({ page }) => {});
+test("date properly formatted", async ({ page }) => {
+  await page.goto(localhost);
 
-test("home page links go to page", async ({ page }) => {});
+  for (const li of await page.getByRole("listitem").getByRole("link").all()) {
+    await li.click();
+    await expect(page.locator("main")).not.toBeEmpty();
+    await page.goto(localhost);
+  }
+});
+
+test("post contains post data", async ({ page }) => {
+  await page.goto(localhost);
+  for (const li of await page.getByRole("listitem").getByRole("link").all()) {
+    await li.click();
+    await expect(page.locator("main>article>div>time")).toContainText(
+      /^[\w\s]+ ([1-9]|[12][0-9]|3[01]), (19|20)\d{2}$/
+    );
+    await page.goto(localhost);
+  }
+});
+
+test("home page links go to page", async ({ page }) => {
+  await page.goto(localhost);
+  for (const li of await page.getByRole("listitem").getByRole("link").all()) {
+    await li.click();
+    await page.getByRole("link").click();
+    await expect(page).toHaveURL(localhost);
+  }
+});
